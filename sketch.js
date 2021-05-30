190ac0d346f0461c38606eb615a4eb331f0746a4
Tejas268 <83995723+Tejas268@users.noreply.github.com>
2021-05-30 13:21:49 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -4,29 +4,38 @@ var END = 2;
 var WIN = 3;
 var gameState  = PLAY;
 
-var monkey, monkey2, monkey_running, monkeyI, V, wall, wall1, monkey_jumping, ground1, ground2, ground3, back, back1, backIMG, groundImage, groundIMG, youWin, youWinIMG, youWinSound;
-var banana ,bananaImage, obstacle, obstacleImage, gameOver, gameOverIMG, gameOverSound, reset, resetImage;
-var foodsGroup, obstaclesGroup;
+// creating variables for monkey
+var monkey, monkey2, monkey_running, monkeyI, monkey_jumping;
+
+var back, back1, youWin, gameOver, rest, youWinSound,  firingSound, invisibleGround ,V, wall, w2;
+// creating Image variables
+var bananaImage, fireImage, gameOverIMG, resetImage, youWinIMG, groundImage, backIMG, uFireImage, grImage;
+ // creating group variables
+var foodsGroup, firesGroup, backGroup, groundGroup, uFireGroup;
 var score, ST, W, H;
-var r = 1
+var r = 1;
+var n = 1;
+
 
 function preload(){
   
   
-  monkey_running = loadAnimation("sprite_0.png","sprite_1.png","sprite_2.png","sprite_3.png","sprite_4.png","sprite_5.png","sprite_6.png","sprite_7.png","sprite_8.png");
+  monkey_running = loadAnimation("sprite_0.png","sprite_1.png","sprite_2.png","sprite_3.png",
+  "sprite_4.png","sprite_5.png","sprite_6.png","sprite_7.png","sprite_8.png");
   monkey_jumping = loadAnimation("sprite_8.png");
   
   bananaImage = loadImage("banana.png");
-  obstacleImage = loadImage("obstacle.png");
+  grImage = loadImage("upground.png")
+  uFireImage = loadImage("ufire.png");
+  fireImage = loadImage("fire.png");
   groundImage = loadImage("ground.png");
   gameOverIMG = loadImage("gameOver.png");
   resetImage = loadImage("reset.png");
   youWinIMG = loadImage("win.gif");
   backIMG = loadImage("back.jpg");
-  groundIMG = loadImage("upground.jpg");
   monkeyI = loadImage("monkey.gif")
 
-  gameOverSound = loadSound("gameover.mp3");
+  firingSound = loadSound("fire.wav");
   youWinSound = loadSound("youwin.mp3");
 
 }
@@ -39,30 +48,23 @@ function setup() {
   H = windowHeight;
   createCanvas(W,H)
   
+  score = 0;
+  ST = 0;
+  
   //crating monkey
   monkey = createSprite(80, H - 126, 20, 20);
   monkey.addAnimation("walking", monkey_running);
   monkey.addAnimation("jumping", monkey_jumping);
-  monkey.scale = 0.1;
+  monkey.scale = 0.12;
   monkey.setCollider("rectangle",0,0,monkey.hight,monkey.width);
-  
-  //creating ground
-  ground1 = createSprite(312,H/1.1,20,20);
-  ground1.addImage(groundImage);
-  ground1.scale = 0.15;
-  ground1.setCollider("rectangle",0,0, ground1.width,ground1.height-200);
-  
-  ground2 = createSprite(934,H/1.1,20,20);
-  ground2.addImage(groundImage);
-  ground2.scale = 0.15;
-  ground2.setCollider("rectangle",0,0, ground2.width,ground2.height-200);
-  
-  ground3 = createSprite(1521,H/1.1,20,20);
-  ground3.addImage(groundImage);
-  ground3.scale = 0.15;
-  ground3.setCollider("rectangle",0,0, ground3.width,ground3.height-200);
 
-  youWin = createSprite(W/3,H/2,20,20);
+  w2 = createSprite(W/4.2,H/2,10,H);
+  w2.visible = false;
+  
+  invisibleGround = createSprite(W/2,H/1.18,W,20);
+  invisibleGround.visible = false;
+
+  youWin = createSprite(W/3.5,H/2,20,20);
   youWin.addImage(youWinIMG);
   youWin.scale = 1.4;
   youWin.visible = false;
@@ -74,30 +76,31 @@ function setup() {
   
   reset = createSprite(W/2,gameOver.y+60,20,20);
   reset.addImage(resetImage);
-  reset.scale = 0.1;
+  reset.scale = 0.3;
   reset.visible = false;
+  
+  backGroup = createGroup();
+  groundGroup = createGroup();
+  foodsGroup = createGroup();
+  firesGroup = createGroup();
+  uFireGroup = createGroup();
+
+  gr();
   
   back = createSprite(W/2,H/2,20,20);
   back.addImage(backIMG);
   back.depth = monkey.depth - 2;
   back.scale = 1.7;
+  backGroup.add(back);
   
   back1 = createSprite(W,H/2,20,20);
   back1.addImage(backIMG);
   back1.depth = monkey.depth - 2;
   back1.scale = 1.7;
+  backGroup.add(back1);
   
-  wall = createSprite(W-30,H/2,10,H);
+  wall = createSprite(W-60,H/2,10,H);
   wall.visible = false;
-  
-  wall1 = createSprite(50,H/2,10,H);
-  wall1.visible = false;
-  
-  foodsGroup = createGroup();
-  obstaclesGroup = createGroup();
-  
-  score = 0;
-  ST = 0;
   
 }
 
@@ -115,24 +118,11 @@ function draw() {
   
   if (gameState === PLAY) {
     
-    if (ground1.x < -311) {
-      ground1.x = 312;
-
-    } if (ground2.x < 313) {
-      ground2.x = 934;
-
-    } if (ground3.x < 900) {
-      ground3.x = 1521;
-    }
     
-    
-if (monkey.isTouching(ground1) || monkey.isTouching(ground2) ||   monkey.isTouching(ground3)) {
+if (monkey.isTouching(invisibleGround)) {
       
     monkey.changeAnimation("walking", monkey_running);
-    monkey.collide(ground1);
-    monkey.collide(ground2);
-    monkey.collide(ground3);
-    monkey.collide(wall1);
+    monkey.collide(invisibleGround);
   
   if(keyDown("space") || touches.length > 0) {
       monkey.velocityY = -19; 
@@ -142,37 +132,45 @@ if (monkey.isTouching(ground1) || monkey.isTouching(ground2) ||   monkey.isTouch
     
     monkey.velocityY = monkey.velocityY + 0.9;
     
-    spawnObstacles();
+    spawnFire();
+    spawnUFire();
     spawnFood();
+    spawnGround();
+    spawnUGround();
     
     if (foodsGroup.isTouching(monkey)) {
       foodsGroup[0].destroy();
       score = score + 1;
     }
     
-    ST = Math.round(frameCount/31);
+    ST = ST + Math.round(frameRate()/31);
     V = -(8 + score/8);
-    ground1.velocityX = V;
-    ground2.velocityX = V;
-    ground3.velocityX = V;
     back.velocityX = V;
     back1.velocityX = V;
     
-    if (obstaclesGroup.isTouching(monkey)) {
+    if (firesGroup.isTouching(monkey)) {
+      gameState = END;
+
+    } else if (uFireGroup.isTouching(monkey)) {
       gameState = END;
     }
     
     if (score >= 30) {
       gameState = WIN;
     }
+
+    if (uFireGroup.isTouching(w2)) {
+      uFireGroup[0].velocityY = (4 + score/10);
+    }
     
   } else if (gameState === END) {
     game();
     gameOver.visible = true;
     reset.visible = true;
+    monkey.visible = false;
     
     if (r  === 1) {
-      gameOverSound.play();
+      firingSound.play();
       r = 2;
     }
     
@@ -182,56 +180,38 @@ if (monkey.isTouching(ground1) || monkey.isTouching(ground2) ||   monkey.isTouch
     
   } else if (gameState === WIN) {
     youWin.visible = true;
-    monkey1 = createSprite (monkey.x,monkey.y,monkey.width,monkey.height);
-    monkey1.addImage(monkeyI);
-    youWin.depth = monkey1.depth +1;
-    monkey.velocityY = monkey.velocityY + 0.9;
-    monkey.velocityX = 4;
-    monkey.collide(wall);
-    monkey.collide(ground1);
-    monkey.collide(ground2);
-    monkey.collide(ground3);
-    monkey.scale = 0.15;
+    
+    if (n === 1) {
+       monkey1 = createSprite (monkey.x,monkey.y,monkey.width,monkey.height);
+      monkey1.addImage(monkeyI);
+      monkey1.setCollider ("rectangle",0,0,monkey1.width,monkey1.height);
+      youWinSound.play();
+    }
+    monkey1.velocityY = 4;
+    monkey1.velocityX = 4;
+    
+    if (monkey1.isTouching(wall)) {
+      monkey1.velocityY = 0;
+      monkey1.velocityX = 0;
+    }
+    monkey1.collide(invisibleGround);
+    groundGroup.collide(monkey);
     
     game();
     restart();
-    gameState = WIN;
   }
    
   drawSprites();
   textSize(17);
   fill("black");
   text("Survival Time: "+ ST, H/5,50);
-  text("Score: "+score,W/1.5,50);
-  
-   if (monkey.x < 0) {
-     textSize(25);
-     fill("red")
-     text("Please reload this game",W/2,H/1.5);
-     gameState = END;
-  }
-  
+  text("Score: "+score,W/1.2,50);
 }
   
-
-function spawnObstacles() {
-  if (frameCount % 160 === 0) {
-    var obstacle = createSprite(W,H-130,20,20);
-    obstacle.addImage(obstacleImage);
-    obstacle.velocityX = V;
-    obstacle.setCollider("circle",0,0,170)
-    obstacle.scale = 0.2;
-    obstacle.depth = monkey.depth -1;
-    obstacle.lifetime = 300;
-    
-    //adding obstacle in Group
-    obstaclesGroup.add(obstacle);
-  }
-}
 
 function spawnFood(){
   if (frameCount % 70 === 0) {
-    var food =  createSprite(W,Math.round(random(H/2,H/3)),20,20);
+    var food =  createSprite(W,Math.round(random(H/1.8,H/2.55)),20,20);
     food.addImage(bananaImage);
     food.velocityX = V;
     food.scale = 0.1;
@@ -243,25 +223,101 @@ function spawnFood(){
 }
 
 function restart() {
-  gameState = PLAY;
-  obstaclesGroup.destroyEach();
+  firesGroup.destroyEach();
   foodsGroup.destroyEach();
+  groundGroup.destroyEach();
+  uFireGroup.destroyEach();
   gameOver.visible = false;
   reset.visible = false;
-  score = 0;
-  r = 1;
+  monkey.visible = true;
+  gr();
+  n = 2;
+  
+  if (gameState === END) {
+    score = 0;
+    ST = 0;
+    r = 1;
+    gameState = PLAY;
+    
+  }
 }
 
 function game() {
-    obstaclesGroup.setLifetimeEach(-1);
+    firesGroup.setLifetimeEach(-1);
     foodsGroup.setLifetimeEach(-1);
-    obstaclesGroup.setVelocityXEach(0);
+    groundGroup.setLifetimeEach(-1);
+    firesGroup.setVelocityXEach(0);
     foodsGroup.setVelocityXEach(0);
+    groundGroup.setVelocityXEach(0);
     monkey.changeAnimation("jumping", monkey_jumping); 
     monkey.velocityY = 0;
-    ground1.velocityX = 0;
-    ground2.velocityX = 0;
-    ground3.velocityX = 0;
     back.velocityX = 0;
     back1.velocityX = 0;
+}
+
+function spawnGround () {
+  if (frameCount % 40 === 0) {
+    var ground = createSprite(W+311,H/1.1,20,20);
+    ground.addImage(groundImage);
+    ground.velocityX = V;
+    ground.lifetime = 300;
+    ground.scale = 0.15;
+    
+    groundGroup.add(ground);
+  }
+}
+
+function gr() {
+  for (var i = 312;i < 1557;i = i + 622) {
+    var ground = createSprite(i,H/1.1,20,20);
+    ground.addImage(groundImage);
+    ground.scale = 0.15;
+    ground.velocityX = -(8 + score/8);
+    ground.lifetime = 300;
+    
+    groundGroup.add(ground);
+  }
+}
+
+function spawnFire() {
+  if (frameCount % 160 === 0) {
+    var fire = createSprite(W + 20,H/1.35,20,20);
+    fire.addImage(fireImage);
+    fire.velocityX = V;
+    fire.setCollider("circle",0,0,fire.width/2-28);
+    fire.scale = 0.6;
+    fire.depth = monkey.depth -1;
+    fire.lifetime = 300;
+    
+    //adding fire in Group
+    firesGroup.add(fire);
+  }
+}
+
+function spawnUFire() {
+  if (frameCount % 220 === 0) {
+    var uFire = createSprite(W + 20,20,20,20);
+    uFire.addImage(uFireImage);
+    uFire.velocityX = -(26 + score/10);
+    uFire.velocityY = (9 + score/10);
+    uFire.setCollider("circle",0,0,uFire.width/2);
+    uFire.scale = 0.3;
+    uFire.depth = monkey.depth -1;
+    uFire.lifetime = 200;
+    
+    //adding up fire in Group
+    uFireGroup.add(uFire);
+  }
+}
+
+function spawnUGround() {
+  if (frameCount % 320 === 0) {
+    var ground = createSprite(W,Math.round(random(H/1.8,H/2)),20,20);
+    ground.addImage(grImage);
+    ground.velocityX = -10;
+    ground.scale = 0.03;
+    ground.setCollider("rectangle",0,0,ground.width,ground.height-80)
+
+    groundGroup.add(ground);
+  }
 }
